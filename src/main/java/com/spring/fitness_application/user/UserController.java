1f74d3b01f4843143d432b89b8cb0d574401261c
marrosuksw <marcelrossi03@gmail.com>
2025-05-24 00:23:38 +0200

    Feat: add register, login, logout endpoints with jwt logic handling

--- src/main/java/com/spring/fitness_application/user/UserController.java
+++ src/main/java/com/spring/fitness_application/user/UserController.java
@@ -1,15 +1,18 @@
 package com.spring.fitness_application.user;
 
+import com.spring.fitness_application.jwt.JwtService;
+import com.spring.fitness_application.personal_data.PersonalData;
+import com.spring.fitness_application.personal_data.PersonalDataService;
+import com.spring.fitness_application.user.dto.LoginRequest;
 import com.spring.fitness_application.user.dto.LoginResponse;
+import com.spring.fitness_application.user.dto.RegisterRequest;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Optional;
-import java.util.UUID;
 
 
 @RestController
@@ -17,25 +20,81 @@ import java.util.UUID;
 public class UserController {
 
     private final UserService userService;
+    private final JwtService jwtService;
+    private final PersonalDataService personalDataService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, JwtService jwtService, PersonalDataService personalDataService) {
         this.userService = userService;
+        this.jwtService = jwtService;
+        this.personalDataService = personalDataService;
     }
-    @GetMapping("")
-    public String login(){
-        return ("this will be a login page");
-    }
-    @GetMapping("/csrf")
-    public ResponseEntity csrf(){
-        return new ResponseEntity<>(HttpStatus.OK);
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest registerRequest) {
+        try {
+            if(!userService.validateCredentials(registerRequest)) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            User user = new User(registerRequest.username(), registerRequest.password());
+            userService.create(user);
+            System.out.println("Before personal data save");
+            personalDataService.save(new PersonalData(
+                    registerRequest.personalDataDTO().height(),
+                    registerRequest.personalDataDTO().weight(),
+                    registerRequest.personalDataDTO().age(),
+                    registerRequest.personalDataDTO().physicalActivity(),
+                    registerRequest.personalDataDTO().gender(),
+                    user
+            ));
+            System.out.println("After personal data save");
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+        catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Error in register, catch section entered.");
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> verifyUser(@RequestBody LoginResponse loginResponse){
-        if(userService.verifyUser(loginResponse)) {
-            return new ResponseEntity<>(loginResponse, HttpStatus.OK);
+    public ResponseEntity<LoginResponse> verifyUser(@RequestBody LoginRequest loginRequest, HttpServletResponse response){
+        Optional<User> user = userService.verifyUser(loginRequest);
+        if(user != null && user.isPresent()) {
+            String accessToken = jwtService.generateAccessToken(user.get().getId());
+            String refreshToken = jwtService.generateRefreshToken(user.get().getId());
+            jwtService.setTokenCookies(response, accessToken, refreshToken);
+            return new ResponseEntity<>(new LoginResponse("User authorized"), HttpStatus.OK);
         }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        System.out.println("Authorization rejected");
+        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     }
 
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletResponse response) {
+        Cookie accessCookie = new Cookie("accessToken", null);
+        accessCookie.setHttpOnly(true);
+        accessCookie.setSecure(true);
+        accessCookie.setPath("/");
+        accessCookie.setMaxAge(0);
+
+        Cookie refreshCookie = new Cookie("refreshToken", null);
+        refreshCookie.setHttpOnly(true);
+        refreshCookie.setSecure(true);
+        refreshCookie.setPath("/");
+        refreshCookie.setMaxAge(0);
+
+        response.addCookie(accessCookie);
+        response.addCookie(refreshCookie);
+
+        return ResponseEntity.ok().build();
+    }
+    @GetMapping("/me")
+    public ResponseEntity<?> verifyCurrentlyLoggedIn(@CookieValue(name = "accessToken", required = false) String accessToken) {
+        if(accessToken != null && !accessToken.isEmpty()) {
+            System.out.println(accessToken);
+            if(jwtService.validateToken(accessToken)) {
+                return new ResponseEntity<>(HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    }
 }
