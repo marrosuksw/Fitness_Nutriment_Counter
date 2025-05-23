0a2295563fa788c35d8ec24bd9d2b459a0013245
marrosuksw <marcelrossi03@gmail.com>
2025-05-24 00:22:05 +0200

    Feat: add register credentials validation and password hashing for safer data storage

--- src/main/java/com/spring/fitness_application/user/UserService.java
+++ src/main/java/com/spring/fitness_application/user/UserService.java
@@ -1,30 +1,41 @@
 package com.spring.fitness_application.user;
 
-import com.spring.fitness_application.user.dto.LoginResponse;
+import com.spring.fitness_application.user.dto.LoginRequest;
+import com.spring.fitness_application.user.dto.RegisterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 
     @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
     }
 
-    public User findById(long id) {
+    public User findById(Long id) {
        return userRepository.findById(id).orElseThrow(()->new RuntimeException("User not found"));
     }
-    public User create(User user) {
-       // user.setPassword(hashPassword(user.getPassword()));
-        return userRepository.save(user);
+    public boolean validateCredentials(RegisterRequest registerRequest) {
+        return !registerRequest.username().isEmpty() && !registerRequest.password().isEmpty()
+                && registerRequest.password().length() > 8 && registerRequest.username().length() > 3
+                && registerRequest.username().length() < 16 && registerRequest.password().length() < 16;
+    }
+    public void create(User user) {
+        if(findByUsername(user.getUsername()).isPresent()) {
+            throw new RuntimeException("Username already exists");
+        }
+        else {
+            user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+            userRepository.save(user);
+        }
     }
     public Optional<User> findByUsername(String username) {
         return userRepository.findByUsername(username);
@@ -32,13 +43,13 @@ public class UserService {
     public void delete(User user) {
         userRepository.delete(user);
     }
-    //TODO return some token or stuff that generates separately, use ''matches'' for encoded password comparison
-    public boolean verifyUser(LoginResponse response){
-        Optional<User> user = findByUsername(response.username());
-        boolean matches = false;
-        if(user.isPresent()){
-            matches = (response.password().equals(user.get().getPassword()));
+    public Optional<User> verifyUser(LoginRequest response){
+        Optional<User> user = userRepository.findByUsername(response.username());
+        String hashedPassword = user.get().getPassword();
+        String responsePassword = response.password();
+        if(user.isPresent() && bCryptPasswordEncoder.matches(responsePassword, hashedPassword)) {
+            return user;
         }
-        return matches;
+        return Optional.empty();
     }
 }
