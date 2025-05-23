865012fdbf65ac6951fc45b2ad3964a2fbb96bea
marrosuksw <marcelrossi03@gmail.com>
2025-05-24 00:11:05 +0200

    Added: Many-To-One relationship with User, FindById Service method, ProductDTO for requests; JWT userId extraction

--- src/main/java/com/spring/fitness_application/product/ProductController.java
+++ src/main/java/com/spring/fitness_application/product/ProductController.java
@@ -1,11 +1,18 @@
 package com.spring.fitness_application.product;
 
+import com.spring.fitness_application.jwt.JwtService;
 import com.spring.fitness_application.product.client.ProductClient;
 import com.spring.fitness_application.product.dto.Product;
+import com.spring.fitness_application.product.dto.ProductDTO;
 import com.spring.fitness_application.product.dto.ProductResponse;
+import com.spring.fitness_application.user.User;
+import com.spring.fitness_application.user.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
@@ -20,30 +27,66 @@ public class ProductController {
 
     private final ProductClient productClient;
     private final ProductService productService;
-
+    private final UserService userService;
+    private final JwtService jwtService;
+    //TODO create a function for getting security context authentication that returns userID
 
     @Autowired
-    public ProductController(ProductClient productClient, ProductService productService) {
+    public ProductController(ProductClient productClient, ProductService productService, UserService userService, JwtService jwtService) {
         this.productClient = productClient;
         this.productService = productService;
+        this.userService = userService;
+        this.jwtService = jwtService;
     }
-    //Search results
+    //Search results endpoint
     @GetMapping("/search")
-    public ResponseEntity<ProductResponse> findByName(@RequestParam String name) {
+    public ResponseEntity<ProductResponse> findByName(@RequestParam String name, HttpServletRequest request) {
+        String token = jwtService.extractTokenFromCookie(request);
+        if(!jwtService.validateToken(token)) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
         System.out.println(productClient.findByName(name));
         return ResponseEntity.ok(productClient.findByName(name));
     }
-    //List of products added to the database by the user
+    // Endpoint with a list of products added to the database by the currently logged in user
     @GetMapping("/list")
-    public ResponseEntity<List<ProductEntity>> findAll(){
-        return ResponseEntity.ok(productService.findAllProducts());
+    public ResponseEntity<List<ProductDTO>> findAllByUserId(HttpServletRequest request) {
+        String token = jwtService.extractTokenFromCookie(request);
+        if (!jwtService.validateToken(token)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        Long id = jwtService.extractId(token);
+        List<ProductEntity> productList = productService.findAllProducts(id);
+        List<ProductDTO> productDTOList = new ArrayList<>();
+        for (ProductEntity productEntity : productList) {
+            productDTOList.add(new ProductDTO(
+                    productEntity.getId(),
+                    productEntity.getName(),
+                    productEntity.getBrands(),
+                    productEntity.getCalories(),
+                    productEntity.getProtein(),
+                    productEntity.getCarbohydrates(),
+                    productEntity.getFat(),
+                    productEntity.getSugar(),
+                    productEntity.getSaturatedFat()
+            ));
+        }
+        return ResponseEntity.ok(productDTOList);
     }
     //Saving a product given by the user; fetched from frontend
     @PostMapping("/add")
     public ResponseEntity<ProductEntity> saveProduct
-            (@RequestBody Product product){
+            (@RequestBody Product product, HttpServletRequest request) {
         try {
+            // TODO to remove or change
+            String token = jwtService.extractTokenFromCookie(request);
+            if(!jwtService.validateToken(token)) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+            // TODO end;
             if (product != null) {
+                Long id = jwtService.extractId(token);
+                User userFromToken = userService.findById(id);
                 ProductEntity productEntity;
                 if (product.getProductNutriments() != null) {
                     productEntity = new ProductEntity(
@@ -54,17 +97,18 @@ public class ProductController {
                             product.getProductNutriments().getCarbohydrates() == null ? BigDecimal.ZERO : product.getProductNutriments().getCarbohydrates(),
                             product.getProductNutriments().getFat() == null ? BigDecimal.ZERO : product.getProductNutriments().getFat(),
                             product.getProductNutriments().getSugar() == null ? BigDecimal.ZERO : product.getProductNutriments().getSugar(),
-                            product.getProductNutriments().getSaturatedFat() == null ? BigDecimal.ZERO : product.getProductNutriments().getSaturatedFat()
+                            product.getProductNutriments().getSaturatedFat() == null ? BigDecimal.ZERO : product.getProductNutriments().getSaturatedFat(),
+                            userFromToken
                     );
                 }
                 else{
                     productEntity = new ProductEntity(
                             product.getProduct_name(),
-                            product.getBrands() == null ? "Unknown" : product.brandListToString()
+                            product.getBrands() == null ? "Unknown" : product.brandListToString(),
+                            userFromToken
                     );
                 }
                 productService.saveProduct(productEntity);
-                System.out.println(productEntity.toString());
                 return new ResponseEntity<>(HttpStatus.CREATED);
 
             }
@@ -77,8 +121,12 @@ public class ProductController {
     }
     //
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<ProductEntity> deleteProduct(@PathVariable Long id) {
+    public ResponseEntity<ProductEntity> deleteProduct(@PathVariable Long id, HttpServletRequest request) {
         try{
+            String token = jwtService.extractTokenFromCookie(request);
+            if(!jwtService.validateToken(token)) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
             Optional<ProductEntity> foundProduct = productService.getProductById(id);
             foundProduct.ifPresent(productService::deleteProduct);
             return new ResponseEntity<>(HttpStatus.OK);
