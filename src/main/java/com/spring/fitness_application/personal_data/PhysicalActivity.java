565a3654212997732741a2f34ba8bec11ce9da1f
marrosuksw <marcelrossi03@gmail.com>
2025-05-24 00:31:00 +0200

    Refactor: change package of physical activity enum for order, remove obsolete user data response

---
+++ src/main/java/com/spring/fitness_application/personal_data/PhysicalActivity.java
@@ -0,0 +1,5 @@
+package com.spring.fitness_application.personal_data;
+
+public enum PhysicalActivity {
+    LOW, LIGHT, MODERATE, HIGH, EXTREME
+}
--- src/main/java/com/spring/fitness_application/user/PhysicalActivity.java
+++
@@ -1,5 +0,0 @@
-package com.spring.fitness_application.user;
-
-public enum PhysicalActivity {
-    LOW, LIGHT, MODERATE, HIGH, EXTREME;
-}
