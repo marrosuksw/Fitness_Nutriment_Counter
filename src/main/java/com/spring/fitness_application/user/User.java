eb2b7a59c4824a389ad55234b84a73caf6a0eb0e
marrosuksw <marcelrossi03@gmail.com>
2025-05-24 00:24:19 +0200

    Feat: add one-to-one relation with personal data, one-to-many relation with products

--- src/main/java/com/spring/fitness_application/user/User.java
+++ src/main/java/com/spring/fitness_application/user/User.java
@@ -1,11 +1,14 @@
 package com.spring.fitness_application.user;
 
+import com.spring.fitness_application.product.ProductEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import com.spring.fitness_application.personal_data.PersonalData;
 
-import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -13,14 +16,18 @@ import java.math.BigDecimal;
 @Table(name = "users")
 @NoArgsConstructor
 public class User {
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     @Column(name = "id", unique = true, updatable = false, nullable = false)
-    private long id;
+    private Long id;
     @Column(name="username", nullable = false)
     private String username;
     @Column(name="password", nullable = false)
     private String password;
+    @OneToOne(mappedBy="user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private PersonalData personalData;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProductEntity> products = new ArrayList<>();
 
     public User(String username, String password) {
         this.username = username;
